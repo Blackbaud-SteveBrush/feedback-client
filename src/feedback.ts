c1e4caa3bdce5cfa44fdb2f882a5a693da526b64
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-07-27 21:41:02 -0400

    Updates

--- src/feedback.ts
+++ src/feedback.ts
@@ -1,13 +1,30 @@
 export class BBFeedback {
   public static load(config: any = {}): void {
 
+    const css = `
+    .sky-omnibar-placeholder {
+      border: 1px solid #000;
+    }
+    `;
+
+    const styleElement = document.createElement('style');
+    styleElement.type = 'text/css';
+    styleElement.appendChild(document.createTextNode(css));
+    document.head.appendChild(styleElement);
+
+    // Create a div.
+    const div = document.createElement('div');
+    div.className = 'sky-omnibar-placeholder';
+
     // Create the iframe.
     const iframeElement: HTMLIFrameElement = document.createElement('iframe');
     iframeElement.frameBorder = '0';
     iframeElement.width = '200px';
     iframeElement.height = '100px';
     iframeElement.setAttribute('src', 'https://host.nxt.blackbaud.com/feedback/');
-    document.body.appendChild(iframeElement);
+
+    div.appendChild(iframeElement);
+    document.body.appendChild(div);
 
     // Listen for messages from the iframe.
     window.addEventListener('message', (event) => {
@@ -17,7 +34,7 @@ export class BBFeedback {
       }
 
       if (event.data.source === 'feedback') {
-        console.log('Message received from iframe!', event.data.message);
+        console.log('Message received from iframe!', event.data);
         iframeElement.contentWindow.postMessage({
           response: {
             url: location.href,
